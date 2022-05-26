02b2f169135a2c7b38f0e15bd2dcabaa119d9b02
Alexandre Geissmann <alex@la-bonaventure.com>
2022-05-26 08:34:23 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import MapboxMap from './MapboxMap.vue';
+</script>
+
+<template>
+<MapboxMap />
+</template>
---
+++ src/MapboxMap.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { type Map } from 'mapbox-gl';
+
+let MyMap:Map
+
+(async () => {
+  MyMap = new (await import('mapbox-gl')).Map
+})();
+</script>
+
+
